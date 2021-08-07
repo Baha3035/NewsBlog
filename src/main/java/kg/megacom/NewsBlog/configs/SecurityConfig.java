47bf31d53fbaa9762097ef7e019513e3f73eaa25
Baha <batmaaanpls@gmail.com>
2021-08-07 13:04:06 +0600

    crfc unabled

--- src/main/java/kg/megacom/NewsBlog/configs/SecurityConfig.java
+++ src/main/java/kg/megacom/NewsBlog/configs/SecurityConfig.java
@@ -1,23 +1,34 @@
 package kg.megacom.NewsBlog.configs;
 
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@EnableWebSecurity
+
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+@EnableWebSecurity
+public class SecurityConfig
+        extends WebSecurityConfigurerAdapter implements WebMvcConfigurer  {
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*");
+        registry.addMapping("/**").allowedMethods("*");
     }
+
+
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("*");
+//    }
 }
